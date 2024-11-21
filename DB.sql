d6173fe57733a4a26ed1cffa3fc33223cda7d724
hammy134 <muhammad78600001@gmail.com>
2024-11-21 17:19:22 +0500

    Update Database

--- DB.sql
+++ DB.sql
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table stream_social_db.comments: ~3 rows (approximately)
+-- Dumping data for table stream_social_db.comments: ~2 rows (approximately)
 INSERT INTO `comments` (`id`, `post_id`, `user_id`, `content`, `created_at`, `updated_at`) VALUES
 	(1, 13, 5, 'Hi Nice Post', '2024-11-15 09:01:54', '2024-11-15 09:01:54'),
 	(2, 13, 5, 'i like it', '2024-11-15 09:04:41', '2024-11-15 09:04:41'),
@@ -66,6 +66,30 @@ CREATE TABLE IF NOT EXISTS `failed_jobs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table stream_social_db.failed_jobs: ~0 rows (approximately)
+
+-- Dumping structure for table stream_social_db.friend_requests
+CREATE TABLE IF NOT EXISTS `friend_requests` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `sender_id` bigint unsigned NOT NULL,
+  `receiver_id` bigint unsigned NOT NULL,
+  `status` enum('pending','accepted','declined') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `friend_requests_sender_id_foreign` (`sender_id`),
+  KEY `friend_requests_receiver_id_foreign` (`receiver_id`),
+  CONSTRAINT `friend_requests_receiver_id_foreign` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `friend_requests_sender_id_foreign` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table stream_social_db.friend_requests: ~6 rows (approximately)
+INSERT INTO `friend_requests` (`id`, `sender_id`, `receiver_id`, `status`, `created_at`, `updated_at`) VALUES
+	(1, 4, 6, 'pending', '2024-11-18 07:48:22', '2024-11-18 07:48:22'),
+	(2, 4, 5, 'accepted', '2024-11-18 07:49:23', '2024-11-18 11:02:39'),
+	(3, 7, 7, 'pending', '2024-11-21 06:03:17', '2024-11-21 06:03:17'),
+	(4, 7, 6, 'pending', '2024-11-21 06:03:20', '2024-11-21 06:03:20'),
+	(5, 7, 5, 'pending', '2024-11-21 06:03:23', '2024-11-21 06:03:23'),
+	(6, 7, 4, 'accepted', '2024-11-21 06:03:26', '2024-11-21 06:03:56');
 
 -- Dumping structure for table stream_social_db.likes
 CREATE TABLE IF NOT EXISTS `likes` (
@@ -110,7 +134,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table stream_social_db.migrations: ~12 rows (approximately)
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -125,7 +149,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(9, '2021_09_28_150804_create_users_permission_table', 2),
 	(10, '2021_09_28_150837_create_users_roles_table', 2),
 	(11, '2021_09_28_151026_create_roles_permissions_table', 2),
-	(12, '2021_10_12_151812_create_posts_table', 2);
+	(12, '2021_10_12_151812_create_posts_table', 2),
+	(13, '2024_11_18_123610_create_friend_requests_table', 3);
 
 -- Dumping structure for table stream_social_db.password_resets
 CREATE TABLE IF NOT EXISTS `password_resets` (
@@ -259,13 +284,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table stream_social_db.users: ~1 rows (approximately)
 INSERT INTO `users` (`id`, `profile_picture`, `first_name`, `last_name`, `username`, `type`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(4, 'user_profiles/1731773359_profile_pic.jpg', 'Hammad', 'Khanam', 'hammy', 'player', 'hammad1@gmail.com', NULL, '$2y$10$9jxvAEt5FKbp10yNjj0jvuI5ulIxoCp1/O8SzL8Bvnb8uJ5eRdzaS', 'zgLDYFk2uYYK5wxvu64ARojbvseCIwZbe9KYEEikZJMpxj31QmRznR9gTN0y', '2024-11-09 07:36:57', '2024-11-16 11:09:19'),
+	(4, 'user_profiles/1731773359_profile_pic.jpg', 'Hammad', 'Khanam', 'hammy', 'player', 'hammad1@gmail.com', NULL, '$2y$10$9jxvAEt5FKbp10yNjj0jvuI5ulIxoCp1/O8SzL8Bvnb8uJ5eRdzaS', 'LzycDnEmTt8ZNLiAKiBYKmZrA2hsfhGQY9Mvj8WWEq3OxLe5yA7SKSYSh5nF', '2024-11-09 07:36:57', '2024-11-16 11:09:19'),
 	(5, 'user_profiles/profile image 3333.jpg', 'Hammad', 'Khan', 'hammad2@gmail.com', 'player', 'hammad2@gmail.com', NULL, '$2y$10$DKzIhBXsY47UF7PeMNSzmeSLYLacXDEapbJP.PAhQrpu1SzDDeIFS', NULL, '2024-11-15 07:34:30', '2024-11-15 07:34:30'),
-	(6, 'user_profiles/new.jpg', 'Ahmed', 'Ali', 'ahmed@gmail.com', 'player', 'ahmed@gmail.com', NULL, '$2y$10$6ngiMNYUemNCxcc5Avpute8EfUhERDvpQqklrTwzK9LpBKNevF1.O', NULL, '2024-11-18 07:08:08', '2024-11-18 07:08:08');
+	(6, 'user_profiles/new.jpg', 'Ahmed', 'Ali', 'ahmed@gmail.com', 'player', 'ahmed@gmail.com', NULL, '$2y$10$6ngiMNYUemNCxcc5Avpute8EfUhERDvpQqklrTwzK9LpBKNevF1.O', NULL, '2024-11-18 07:08:08', '2024-11-18 07:08:08'),
+	(7, 'user_profiles/profile image 3333.jpg', 'Shahrukh', 'Arain', 'shahrukh', 'player', 'hammad@gmail.com', NULL, '$2y$10$OA6fl/OExLHK7CyDUN81CekLTXi5YfDbDYLhzz79SfOwPRrzYDU9C', NULL, '2024-11-21 06:02:58', '2024-11-21 06:02:58');
 
 -- Dumping structure for table stream_social_db.users_permissions
 CREATE TABLE IF NOT EXISTS `users_permissions` (
