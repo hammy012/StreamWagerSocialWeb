fbdde2a0aaa8930f022baef0c29cc63c75168b99
hammy134 <muhammad78600001@gmail.com>
2024-12-07 14:58:18 +0500

    Setting Reset Password Page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,7 +2,8 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img decoding="async" style="width: 100px;" src="{{ asset('assets/img/logo.jpg') }}"
+                    class="attachment-thumbnail size-thumbnail wp-image-837" alt="" />
             </a>
         </x-slot>
 
